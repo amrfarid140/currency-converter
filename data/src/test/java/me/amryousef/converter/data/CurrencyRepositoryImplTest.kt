d16924f3c685ba113a6abf507912233f47429593
Amr Yousef <amr.farid140@gmail.com>
2019-08-18 23:08:46 +0100

    Added CurrencyRepositoryImplTest

--- data/src/test/java/me/amryousef/converter/data/CurrencyRepositoryImplTest.kt
+++ data/src/test/java/me/amryousef/converter/data/CurrencyRepositoryImplTest.kt
@@ -1,5 +1,50 @@
 package me.amryousef.converter.data
 
+import com.google.gson.JsonSyntaxException
+import com.nhaarman.mockitokotlin2.given
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
+import io.reactivex.Observable
+import me.amryousef.converter.domain.CurrencyRate
+import me.amryousef.converter.domain.CurrencyRepository
+import me.amryousef.converter.domain.WritableCurrencyRepository
+import org.junit.Test
+
 class CurrencyRepositoryImplTest {
-    
+    private val mockLocalRepository =
+        mock<WritableCurrencyRepository>()
+    private val mockRemoteRepository =
+        mock<CurrencyRepository>()
+
+    private val repositoryImpl = CurrencyRepositoryImpl(
+        localRepository = mockLocalRepository,
+        remoteRepository =  mockRemoteRepository
+    )
+
+    @Test
+    fun givenRemoteRepositoryHasData_WhenObserveCurrencyRates_ThenLocalRepositoryIsUpdated() {
+        // Given
+        val mockCurrencyData = listOf(mock<CurrencyRate>())
+        given(mockRemoteRepository.observeCurrencyRates())
+            .willReturn(Observable.just(mockCurrencyData))
+
+        // When
+        repositoryImpl.observeCurrencyRates().test()
+
+        // Then
+        verify(mockLocalRepository).addCurrencyRates(mockCurrencyData)
+    }
+
+    @Test
+    fun givenRemoteRepositoryErrors_WhenObserveCurrencyRates_ThenLocalDataIsReturned() {
+        // Given
+        given(mockRemoteRepository.observeCurrencyRates())
+            .willReturn(Observable.error(JsonSyntaxException("Test")))
+
+        // When
+        repositoryImpl.observeCurrencyRates().test()
+
+        // Then
+        verify(mockLocalRepository).observeCurrencyRates()
+    }
 }

078f2ff1d11a562acc1d1da72cb652e08c090557
Amr Yousef <amr.farid140@gmail.com>
2020-06-27 20:51:16 +0100

    Fix remote repository tests

--- data/src/test/java/me/amryousef/converter/data/RemoteCurrencyRepositoryTest.kt
+++ data/src/test/java/me/amryousef/converter/data/RemoteCurrencyRepositoryTest.kt
@@ -3,17 +3,19 @@ package me.amryousef.converter.data
 import com.google.gson.JsonSyntaxException
 import com.nhaarman.mockitokotlin2.*
 import io.reactivex.Single
-import io.reactivex.schedulers.TestScheduler
+import io.reactivex.schedulers.Schedulers
 import me.amryousef.converter.data.remote.CurrencyRatesService
 import me.amryousef.converter.data.remote.RemoteCurrencyRepository
 import me.amryousef.converter.data.remote.RemoteCurrencyRepositoryMapper
+import me.amryousef.converter.domain.CountryRepository
 import me.amryousef.converter.domain.SchedulerProvider
 import org.junit.Test
 
 class RemoteCurrencyRepositoryTest {
     private val mockMapper = mock<RemoteCurrencyRepositoryMapper>()
     private val mockApiService = mock<CurrencyRatesService>()
-    private val testScheduler = TestScheduler()
+    private val mockCountryRepository = mock<CountryRepository>()
+    private val testScheduler = Schedulers.trampoline()
     private val mockScheduler = mock<SchedulerProvider> {
         on { io() } doReturn testScheduler
         on { main() } doReturn testScheduler
@@ -21,35 +23,54 @@ class RemoteCurrencyRepositoryTest {
     private val remoteRepository = RemoteCurrencyRepository(
         apiService = mockApiService,
         mapper = mockMapper,
-        schedulerProvider = mockScheduler
+        schedulerProvider = mockScheduler,
+        countryRepository = mockCountryRepository
     )
+
+    @Test
+    fun givenServices_WhenObserveCurrencyRates_ThenBothServicesConsumed() {
+        // Given
+        given(mockApiService.getLatestRates())
+            .willReturn(Single.just(emptyMap()))
+        given(mockCountryRepository.getCountryFlagUrl())
+            .willReturn(Single.just(emptyMap()))
+
+        // When
+        remoteRepository.observeCurrencyRates().test()
+
+        //Then
+        verify(mockApiService).getLatestRates()
+        verify(mockCountryRepository).getCountryFlagUrl()
+    }
 
     @Test
     fun givenApiServiceReturnsData_WhenObserveCurrencyRates_ThenApiDataIsMapped() {
         // Given
         given(mockApiService.getLatestRates())
             .willReturn(Single.just(emptyMap()))
+        given(mockCountryRepository.getCountryFlagUrl())
+            .willReturn(Single.just(emptyMap()))
 
         // When
         remoteRepository.observeCurrencyRates().test()
-        testScheduler.triggerActions()
 
         // Then
-        verify(mockMapper).map(any())
+        verify(mockMapper).map(any(), any())
     }
 
     @Test
     fun givenMapperThrowsError_WhenObserveCurrencyRates_ThenOnErrorIsInvoked() {
         // Given
-        given(mockMapper.map(any()))
+        given(mockMapper.map(any(), any()))
             .willThrow(JsonSyntaxException("Error"))
         given(mockApiService.getLatestRates())
+            .willReturn(Single.just(emptyMap()))
+        given(mockCountryRepository.getCountryFlagUrl())
             .willReturn(Single.just(emptyMap()))
 
         // When
         val observer = remoteRepository.observeCurrencyRates()
             .test()
-        testScheduler.triggerActions()
 
         // Then
         observer.assertFailure(JsonSyntaxException::class.java)

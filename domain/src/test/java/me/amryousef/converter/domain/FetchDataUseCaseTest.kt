4be37e93c10ee2594383d503b7c70b0689f931d3
Amr Yousef <amr.farid140@gmail.com>
2020-06-27 21:07:23 +0100

    Simplified UseCase test

--- domain/src/test/java/me/amryousef/converter/domain/FetchDataUseCaseTest.kt
+++ domain/src/test/java/me/amryousef/converter/domain/FetchDataUseCaseTest.kt
@@ -1,44 +1,25 @@
 package me.amryousef.converter.domain
 
 import com.nhaarman.mockitokotlin2.*
-import io.reactivex.Completable
 import io.reactivex.Observable
-import io.reactivex.Single
 import io.reactivex.schedulers.Schedulers
-import io.reactivex.schedulers.TestScheduler
 import org.junit.Before
-import org.junit.Ignore
 import org.junit.Test
-import java.util.Currency
-import java.util.concurrent.TimeUnit
-import kotlin.test.assertEquals
-import kotlin.test.assertNotNull
 import kotlin.test.assertTrue
 
 private typealias Callback<T> = (UseCaseResult<T>) -> Unit
 
 class FetchDataUseCaseTest {
-    
-    private companion object {
-        const val SOME_CURRENCY_CODE = "EGP"
-        val SOME_COUNTRY_DATA = mapOf(
-            "EGP" to "EGY",
-            "EUR" to "ESP",
-            "EUR" to "FRA"
-        )
-    }
-    
-    private val testScheduler = TestScheduler()
+
+    private val testScheduler = Schedulers.trampoline()
     private val mockCurrencyRepository = mock<CurrencyRepository>()
-    private val mockCountryRepository = mock<CountryRepository>()
     private val mockSchedulerProvider = mock<SchedulerProvider> {
         on { io() } doReturn testScheduler
-        on { main() } doReturn Schedulers.trampoline()
+        on { main() } doReturn testScheduler
     }
 
     private val useCase = FetchDataUseCase(
         mockCurrencyRepository,
-        mockCountryRepository,
         mockSchedulerProvider
     )
 
@@ -56,38 +37,10 @@ class FetchDataUseCaseTest {
         given(mockCurrencyRepository.observeCurrencyRates()).willReturn(
             Observable.just(mockData)
         )
-        given(mockCountryRepository.getCountryFlagUrl()).willReturn(
-            Single.just(SOME_COUNTRY_DATA)
-        )
 
         useCase.execute(onResult = mockCallback)
-        testScheduler.triggerActions()
 
         assertTrue(callbackResultCaptor.lastValue is UseCaseResult.Success)
-    }
-
-    @Test
-    fun givenCurrencyIsEuro_WhenExecute_ThenResultHasEUFlagUrl() {
-        val mockData = givenMockEuroCurrencyRates()
-        given(mockCurrencyRepository.observeCurrencyRates()).willReturn(
-            Observable.just(mockData)
-        )
-        given(mockCountryRepository.getCountryFlagUrl()).willReturn(
-            Single.just(SOME_COUNTRY_DATA)
-        )
-
-        useCase.execute(onResult = mockCallback)
-        testScheduler.triggerActions()
-
-        val actualValue = callbackResultCaptor.lastValue
-        assertTrue(actualValue is UseCaseResult.Success)
-        val actualData = actualValue.data.firstOrNull()
-        assertNotNull(actualData)
-        assertEquals(
-            actual = actualData.countryFlagUrl,
-            expected = "https://www.countryflags.io/eu/flat/64.png"
-        )
-
     }
 
     @Test
@@ -97,20 +50,15 @@ class FetchDataUseCaseTest {
         )
 
         useCase.execute(onResult = mockCallback)
-        testScheduler.triggerActions()
 
         assertTrue(callbackResultCaptor.lastValue is UseCaseResult.Error)
     }
 
-    private fun givenMockEuroCurrencyRates() = listOf(
-        mock<CurrencyRate> {
-            on { currency } doReturn Currency.getInstance("EUR")
-        }
-    )
-
     private fun givenMockCurrencyRates() = listOf(
-        mock<CurrencyRate> {
-            on { currency } doReturn Currency.getInstance(SOME_CURRENCY_CODE)
-        }
+        CurrencyRate(
+            currency = CurrencyMetadata("EUR","test"),
+            isBase = true,
+            rate = 22.2
+        )
     )
 }

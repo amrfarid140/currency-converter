81d83605dd2837c12f72a7eb0185fc11c39a5259
Amr Yousef <amr.farid140@gmail.com>
2020-06-27 21:04:30 +0100

    Added more tests to CurrencyRepositoryImplTest

--- data/src/test/java/me/amryousef/converter/data/CurrencyRepositoryImplTest.kt
+++ data/src/test/java/me/amryousef/converter/data/CurrencyRepositoryImplTest.kt
@@ -1,12 +1,10 @@
 package me.amryousef.converter.data
 
 import com.google.gson.JsonSyntaxException
-import com.nhaarman.mockitokotlin2.doReturn
-import com.nhaarman.mockitokotlin2.given
-import com.nhaarman.mockitokotlin2.mock
-import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.*
 import io.reactivex.Observable
-import io.reactivex.schedulers.TestScheduler
+import io.reactivex.schedulers.Schedulers
+import io.reactivex.subjects.PublishSubject
 import me.amryousef.converter.domain.CurrencyRate
 import me.amryousef.converter.domain.CurrencyRepository
 import me.amryousef.converter.domain.SchedulerProvider
@@ -18,7 +16,7 @@ class CurrencyRepositoryImplTest {
         mock<WritableCurrencyRepository>()
     private val mockRemoteRepository =
         mock<CurrencyRepository>()
-    private val testScheduler = TestScheduler()
+    private val testScheduler = Schedulers.trampoline()
     private val mockScheduler = mock<SchedulerProvider> {
         on { io() } doReturn testScheduler
         on { main() } doReturn testScheduler
@@ -41,7 +39,6 @@ class CurrencyRepositoryImplTest {
 
         // When
         repositoryImpl.observeCurrencyRates().test()
-        testScheduler.triggerActions()
 
         // Then
         verify(mockLocalRepository).addCurrencyRates(mockCurrencyData)
@@ -57,9 +54,41 @@ class CurrencyRepositoryImplTest {
 
         // When
         repositoryImpl.observeCurrencyRates().test()
-        testScheduler.triggerActions()
 
         // Then
         verify(mockLocalRepository).observeCurrencyRates()
+    }
+
+    @Test
+    fun givenCurrencyRatesObserved_WhenObserverSubscribed_ThenRemoteDataObserved() {
+        // Given
+        given(mockRemoteRepository.observeCurrencyRates())
+            .willReturn(Observable.just(emptyList()))
+        given(mockLocalRepository.observeCurrencyRates())
+            .willReturn(Observable.just(emptyList()))
+        val observable = repositoryImpl.observeCurrencyRates()
+
+        // When
+        observable.test()
+
+        //Then
+        verify(mockRemoteRepository).observeCurrencyRates()
+    }
+
+    @Test
+    fun givenRemoteRepositoryObserved_WhenDataReceived_ThenDataStoredLocally() {
+        // Given
+        val subject = PublishSubject.create<List<CurrencyRate>>()
+        given(mockRemoteRepository.observeCurrencyRates())
+            .willReturn(subject)
+        given(mockLocalRepository.observeCurrencyRates())
+            .willReturn(Observable.just(emptyList()))
+        repositoryImpl.observeCurrencyRates().test()
+
+        // When
+        subject.onNext(emptyList())
+
+        //Then
+        verify(mockLocalRepository).addCurrencyRates(any())
     }
 }

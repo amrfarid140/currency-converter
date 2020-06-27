628d265895475301c7beaab7e42dcd0b5097e8be
Amr Yousef <amr.farid140@gmail.com>
2020-06-27 23:27:54 +0100

    Add repeated request

--- data/src/main/java/me/amryousef/converter/data/CurrencyRepositoryImpl.kt
+++ data/src/main/java/me/amryousef/converter/data/CurrencyRepositoryImpl.kt
@@ -1,27 +1,45 @@
 package me.amryousef.converter.data
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.launch
 import me.amryousef.converter.domain.CurrencyRepository
 import me.amryousef.converter.domain.SchedulerProvider
 import me.amryousef.converter.domain.WritableCurrencyRepository
 import javax.inject.Inject
 import javax.inject.Named
+import kotlin.coroutines.CoroutineContext
 
 class CurrencyRepositoryImpl @Inject constructor(
     @Named("local") private val localRepository: WritableCurrencyRepository,
     @Named("remote") private val remoteRepository: CurrencyRepository,
     private val schedulerProvider: SchedulerProvider
-) : CurrencyRepository {
+) : CurrencyRepository, CoroutineScope {
+
+    private var job: Job? = null
+    override val coroutineContext: CoroutineContext
+        get() = schedulerProvider.io()
+
+    private fun startRemote() {
+        job = launch {
+            remoteRepository.observeCurrencyRates()
+                .collect {
+                    localRepository.addCurrencyRates(it)
+                }
+        }
+    }
 
     override fun observeCurrencyRates() =
         localRepository.observeCurrencyRates()
             .onStart {
-                //TODO: Fix Repeate
-                remoteRepository.observeCurrencyRates()
-                    .collect {
-                        localRepository.addCurrencyRates(it)
-                    }
-            }.flowOn(schedulerProvider.main())
+                startRemote()
+                emit(emptyList())
+            }.onCompletion {
+                job?.cancel()
+            }
+            .flowOn(schedulerProvider.main())
 }
--- data/src/main/java/me/amryousef/converter/data/remote/RemoteCurrencyRepository.kt
+++ data/src/main/java/me/amryousef/converter/data/remote/RemoteCurrencyRepository.kt
@@ -1,11 +1,15 @@
 package me.amryousef.converter.data.remote
 
 
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.FlowCollector
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 import me.amryousef.converter.domain.CountryRepository
+import me.amryousef.converter.domain.CurrencyRate
 import me.amryousef.converter.domain.CurrencyRepository
 import me.amryousef.converter.domain.SchedulerProvider
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 class RemoteCurrencyRepository @Inject constructor(
@@ -15,9 +19,14 @@ class RemoteCurrencyRepository @Inject constructor(
     private val schedulerProvider: SchedulerProvider
 ) : CurrencyRepository {
 
-    override fun observeCurrencyRates() = flow {
-        val latestRates = apiService.getLatestRates()
-        val flags = countryRepository.getCountryFlagUrl()
-        emit(mapper.map(latestRates, flags))
+    override fun observeCurrencyRates() = flow<List<CurrencyRate>> {
+        repeated(countryRepository.getCountryFlagUrl())
     }.flowOn(schedulerProvider.io())
+
+    private suspend fun FlowCollector<List<CurrencyRate>>.repeated(flags: Map<String, String>) {
+        val latestRates = apiService.getLatestRates()
+        emit(mapper.map(latestRates, flags))
+        delay(TimeUnit.SECONDS.toMillis(5))
+        repeated(flags)
+    }
 }
